2c33ca93d35257376b903b8abc53659c66035805
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2020-05-09 14:26:12 +0800

    add scrollback

--- config.h
+++ config.h
@@ -192,6 +192,11 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY,            	XK_u,     	kscrollup,      {.i =  1} },
+	{ MODKEY,            	XK_j,   	kscrolldown,    {.i =  1} },
+	{ MODKEY|ControlMask,   XK_u,     	kscrollup,      {.i = -1} },
+	{ MODKEY|ControlMask,   XK_j,   	kscrolldown,    {.i = -1} },
+
 };
 
 /*
